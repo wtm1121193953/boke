8cde5130da6d38d9a8d037d8bd91f37212d99728
13148872522 <13148872522@adc.com>
2019-04-23 18:56:12 +0800

    主体框架

---
+++ modules/api/controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+
+  public function actionIndex(){
+      echo  111;
+  }
+
+
+}
