dc10058b2a0f031dfb2d4edb272f74be8b2268f4
wtm1121193953 <1121193953@qq.com>
2019-04-24 21:09:26 +0800

    封装方法

---
+++ modules/api/models/Test.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 13148872522
+ * Date: 2019/4/24
+ * Time: 17:01
+ */
+
+namespace app\modules\api\models;
+
+use app\models\BaseModel;
+
+
+Class Test extends BaseModel{
+
+
+    public  static $table = 'test';
+
+    public function test(){
+
+    }
+
+}
